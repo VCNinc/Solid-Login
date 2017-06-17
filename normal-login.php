e3643ea9a39c69c71bc0cfbb9de8e141da4abe09
Vivek Nair <vnair746@stanford.edu>
2017-06-17 13:24:56 +0800

    Basic Standard Login Backend
    Insecure on Purpose, to demonstrate how SolidLogin makes it secure.

--- normal-login.php
+++ normal-login.php
@@ -1,5 +1,28 @@
 <?php
+$users = [
+	"email@example.com" => "password123"
+];
 
+// This login form is insecure.
+// We know. That's the point.
+// When you add SolidLogin, it becomes perfectly secure.
+
+if(!empty($_POST)) {
+	$error = "";
+	if(!isset($_POST['email']) || empty($_POST['email'])) {
+		$error = "Please enter your email address.";
+	} else if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+		$error = "Please enter a valid email address.";
+	} else if (!isset($users[$_POST['email']])) {
+		$error = "There's no user matching " . $_POST['email'];
+	} else if(!isset($_POST['password']) || empty($_POST['password'])) {
+		$error = "Please enter your password.";
+	} else if($users[$_POST['email']] != $_POST['password']) {
+		$error = "That's not the correct password.";
+	} else {
+		$logged_in = $_POST['email'];
+	}
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,12 +45,32 @@
 			<i class="fa fa-bars" aria-hidden="true"></i>
 		</div>
 		<div class="login-form">
-			<h1>Login to Your Account</h1>
-			<form action="" method="post" id="login-form">
-				<input type="text" placeholder="Email Address" name="email">
-				<input type="password" placeholder="Password" name="password">
-				<button type="submit">Login</button>
-			</form>
+			<?php
+			if(empty($logged_in)) {
+			?>
+				<h1>Login to Your Account</h1>
+				<?php
+				if(!empty($error)) {
+				?>
+				<div class="error">
+					<p><i class="fa fa-exclamation-circle"></i> &nbsp; <?=$error?></p>
+				</div>
+				<?php
+				}
+				?>
+				<form action="" method="post" id="login-form">
+					<input type="text" placeholder="Email Address" name="email">
+					<input type="password" placeholder="Password" name="password">
+					<button type="submit">Login</button>
+				</form>
+			<?php
+			} else {
+			?>
+				<h1 class="mb0">Welcome back, <b><?=$logged_in?></b>.</h1>
+				<p class="inf">You are now logged in to theCompany website.</p>
+			<?php
+			}
+			?>
 		</div>
 	</body>
 </html>
