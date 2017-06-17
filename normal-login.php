5ac19a2b483853ba48f3addc6fdca0453f4f9997
Vivek Nair <vnair746@stanford.edu>
2017-06-17 12:29:39 +0800

    Basic Navbar
    We're creating an example company website to show how SolidLogin could be used

--- normal-login.php
+++ normal-login.php
@@ -8,13 +8,17 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Normal Login Page</title>
-		<link href="assets/normal-login.css" rel="stylesheet">
+		<link rel="stylesheet" href="assets/normal-login.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<!--[if lt IE 9]>
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
 	</head>
 	<body>
-
+		<div class="navbar">
+			<img src="assets/generic-logo.png">
+			<i class="fa fa-bars" aria-hidden="true"></i>
+		</div>
 	</body>
 </html>
