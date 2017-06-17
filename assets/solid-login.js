fd8291f48e6c8e429a6d3ccf5f5f0c9220bd2456
Vivek Nair <vnair746@stanford.edu>
2017-06-17 13:35:06 +0800

    Include JQuery Async

---
+++ assets/solid-login.js
@@ -0,0 +1,10 @@
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.async = true;
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js';
+(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(script);
+script.onload = function() {
+	$(function(){
+		
+	});
+}
