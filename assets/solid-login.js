600e982f66e4937702a7b4b00390c6cf25cf0131
Vivek Nair <vnair746@stanford.edu>
2017-06-17 14:03:54 +0800

    Setup Basic Script to Replace Login form with SolidLogin Form

--- assets/solid-login.js
+++ assets/solid-login.js
@@ -5,6 +5,11 @@ script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js'
 (document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(script);
 script.onload = function() {
 	$(function(){
-		
+		$("head").append('<link rel="stylesheet" href="assets/solid-login.css">');
+		$("#login-form input").hide();
+		$("#login-form").prepend('<div class="solid-login-btn"><img src="assets/solid-login.png"><p>Login Securely with SolidLogin</p><i class="fa fa-chevron-right"></i></div>');
+		$("#login-form").click(function(){
+			alert("Start Secure Login!");
+		});
 	});
 }
