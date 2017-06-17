ea7c931f4335a6814890064f2d1aa1187cdbdce8
Vivek Nair <vnair746@stanford.edu>
2017-06-17 16:44:41 +0800

    Introduce Debug View

--- debug-login.php
+++ debug-login.php
@@ -35,8 +35,9 @@ $password = empty($_POST['password']) ? '' : $_POST['password'];
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Normal Login Page</title>
 		<link rel="stylesheet" href="assets/normal-login.css">
-		<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,600" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="assets/solid-login.css">
 		<!--[if lt IE 9]>
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
@@ -44,7 +45,7 @@ $password = empty($_POST['password']) ? '' : $_POST['password'];
 	</head>
 	<body>
 		<div class="navbar">
-			<img src="assets/generic-logo.png">
+			<img src="assets/generic-logo.svg">
 			<i class="fa fa-bars" aria-hidden="true"></i>
 		</div>
 		<div class="login-form">
@@ -75,5 +76,45 @@ $password = empty($_POST['password']) ? '' : $_POST['password'];
 			}
 			?>
 		</div>
+		<div class="solid-login-modal"></div>
+		<div class="solid-dialogs">
+			<div class="solid-login-dialog">
+				<div class="solid-login-bloq">
+					<img src="assets/solid-login-ico.svg">
+					<i class="fa fa-times-circle"></i>
+					<div class="solid-login-steps"></div>
+					<a href="#" class="solid-login-btn-solid">Next</a>
+					<div class="stepview">
+						<div class="longstep"></div>
+						<div class="step"></div>
+					</div>
+				</div>
+			</div>
+			<div class="solid-debug-dialog">
+				<div class="assesment">
+					<h1>Security Assesment</h1>
+				</div>
+			</div>
+		</div>
+		<script type="text/javascript">
+			var script = document.createElement('script');
+			script.type = 'text/javascript';
+			script.async = true;
+			script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js';
+			(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(script);
+			script.onload = function() {
+				$(function(){
+					$("#login-form input").hide();
+					$("#login-form").prepend('<div class="solid-login-btn"><img src="assets/solid-login.svg" height="25"><p>Login Securely with SolidLogin</p><i class="fa fa-chevron-right"></i></div>');
+					$("button[type=submit]").prop("disabled", true);
+					$(".solid-login-steps").append('<div class="solid-login-step"><i class="fa fa-circle-thin fa-fw"></i><div class="step-text"><p>Step 1</p><h2>Basic Login Credentials</h2></div></div>');
+					$(".solid-login-steps").append('<div class="current-step"><form action="" method="post"><input type="text" placeholder="Email Address" name="email"><input type="password" placeholder="Password" name="password"></form></div>');
+					$("#login-form").click(function(){
+						$(".solid-dialogs").show().animate({marginTop: 0, opacity: 1}, 300);
+						$(".solid-login-modal").fadeIn(300);
+					});
+				});
+			}
+		</script>
 	</body>
 </html>
