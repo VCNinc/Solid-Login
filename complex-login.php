9fa28003e771ee87ab2ef13be0f7c2978d155a8c
Vivek Nair <vnair746@stanford.edu>
2017-06-18 08:35:26 +0800

    Added Complex Login for Fun
    To show how a "secure" login would work without Solid2FA

--- complex-login.php
+++ complex-login.php
@@ -41,7 +41,7 @@ $password = empty($_POST['password']) ? '' : $_POST['password'];
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Normal Login Page</title>
+		<title>Complex Login Page</title>
 		<link rel="stylesheet" href="assets/normal-login.css">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:400,500,600" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -49,6 +49,7 @@ $password = empty($_POST['password']) ? '' : $_POST['password'];
 		  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 		  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
+		<script src='https://www.google.com/recaptcha/api.js'></script>
 	</head>
 	<body>
 		<div class="navbar">
@@ -72,6 +73,34 @@ $password = empty($_POST['password']) ? '' : $_POST['password'];
 				<form action="" method="post" id="login-form">
 					<input type="text" placeholder="Email Address" name="email" value="<?=$email?>">
 					<input type="password" placeholder="Password" name="password" value="<?=$password?>">
+					<hr>
+					<h4>Please answer Security Question 1:</h4>
+					<input type="text" placeholder="Your Answer">
+					<h4>Please answer Security Question 2:</h4>
+					<input type="text" placeholder="Your Answer">
+					<h4>Please answer Security Question 3:</h4>
+					<input type="text" placeholder="Your Answer">
+					<hr>
+					<h4>What is your date of birth?</h4>
+					<input type="text" placeholder="DD/MM/YYYY">
+					<h4>When did you start using our service?</h4>
+					<input type="text" placeholder="DD/MM/YYYY">
+					<hr>
+					<h4>Please verify that you are not a robot.</h4>
+					<div class="g-recaptcha" data-sitekey="6LfK3iUUAAAAAHjv7jnrnDFh8xTqXm5oqX1P0EES"></div>
+					<hr>
+					<h4>Please enter the 2FA code sent to you via SMS.</h4>
+					<input type="text" placeholder="Enter the 6-Digit Code">
+					<h4>Please enter the 2FA code sent to you via Email.</h4>
+					<input type="text" placeholder="Enter the 6-Digit Code">
+					<hr>
+					<h4>Please verify the billing address on file.</h4>
+					<input type="text" placeholder="Address Line 1">
+					<input type="text" placeholder="Address Line 2">
+					<input type="text" placeholder="City, State, and ZIP Code">
+					<hr>
+					<h4>Please verify the Visa credit card number on file (4**************12).</h4>
+					<input type="text" placeholder="Enter your 16-Digit Card Number">
 					<button type="submit">Login</button>
 				</form>
 			<?php
--- debug-login.php
+++ debug-login.php
@@ -33,7 +33,7 @@ $password = empty($_POST['password']) ? '' : $_POST['password'];
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Normal Login Page</title>
+		<title>Debug Login Page</title>
 		<link rel="stylesheet" href="assets/normal-login.css">
 		<link href="https://fonts.googleapis.com/css?family=Roboto:400,500" rel="stylesheet">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
